5f9b42f54a97371d98f3fac35aebdb6d1ddb7845
Gerrit Visser <gerrit-ml@psgv.ca>
2012-11-26 03:29:14 +0000

    Add Callblock to dev branch

---
+++ callblock/app_config.php
@@ -0,0 +1,84 @@
+<?php
+	//application details
+		$apps[$x]['name'] = 'Call Block';
+		$apps[$x]['uuid'] = '9ed63276-e085-4897-839c-4f2e36d92d6c';
+		$apps[$x]['category'] = 'Switch';
+		$apps[$x]['subcategory'] = '';
+		$apps[$x]['version'] = '';
+		$apps[$x]['license'] = 'Mozilla Public License 1.1';
+		$apps[$x]['url'] = 'http://www.fusionpbx.com';
+		$apps[$x]['description']['en'] = 'A tool to block incoming numbers.';
+
+	//menu details
+		$apps[$x]['menu'][0]['title']['en'] = 'Call Block';
+		$apps[$x]['menu'][0]['uuid'] = '29295c90-b1b9-440b-9c7E-c8363c6e8975';
+		$apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
+		$apps[$x]['menu'][0]['category'] = 'internal';
+		$apps[$x]['menu'][0]['path'] = '/app/callblock/callblock.php';
+		$apps[$x]['menu'][0]['groups'][] = 'user';
+		$apps[$x]['menu'][0]['groups'][] = 'admin';
+		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+
+	//permission details
+		$y = 0;
+		$apps[$x]['permissions'][$y]['name'] = 'callblock_view';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'user';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'callblock_add';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'user';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'callblock_edit';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'user';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'callblock_delete';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$apps[$x]['permissions'][$y]['groups'][] = 'user';
+		$y++;
+	//schema details
+		$y = 0; //table array index
+		$z = 0; //field array index
+		$apps[$x]['db'][$y]['table'] = 'v_callblock';
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'blocked_caller_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'blocked_caller_name';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Enter the name.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'blocked_caller_number';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Enter the full phone number.';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'blocked_call_count';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'numeric';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Number of calls.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'blocked_call_action';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Action for call.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'date_added';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Date/Time number was added.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'block_call_enabled';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = 'Enable/disable blocking the call.';
+		$z++;
+?>
---
+++ callblock/app_languages.php
@@ -0,0 +1,72 @@
+﻿<?php
+// callblock.php
+	$text['title-callblock']['en-us'] = 'Call Block';
+
+	$text['description-callblock']['en-us'] = 'A list of numbers from which to block calls.';
+
+	$text['label-number']['en-us'] = 'Number';
+	
+	$text['label-name']['en-us'] = 'Name';
+	
+	$text['label-Count']['en-us'] = 'Count';
+	
+	$text['label-date-added']['en-us'] = 'Date Added';
+	
+	$text['label-action']['en-us'] = 'Action';
+	
+	$text['label-enabled']['en-us'] = 'Enabled';
+	
+	$text['button-edit']['en-us'] = 'Edit';
+
+	$text['button-delete']['en-us'] = 'Delete';
+	
+	$text['button-add']['en-us'] = 'Add';
+
+	$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+	
+	// callblock_cdr_add.php	
+	$text['label-add-complete']['en-us'] = 'Add Complete';
+	
+	// callblock_delete.php
+	$text['label-message']['en-us'] = 'Message';
+
+	$text['label-delete-complete']['en-us'] = 'Delete Complete';
+	
+	// callblock_edit.php
+	$text['label-provide-name']['en-us'] = 'Please provide: Name';
+
+	$text['label-provide-number']['en-us'] = 'Please provide: Number';
+
+	$text['label-provide-enabled']['en-us'] = 'Please provide: Enabled';
+
+	$text['label-update-complete']['en-us'] = 'Update Complete';
+	
+	$text['label-called-on']['en-us'] = 'Called on';
+	
+	$text['label-edit-add']['en-us'] = 'Call Block Add';
+	
+	$text['label-edit-edit']['en-us'] = 'Call Block Edit';
+	
+	$text['label-add-note']['en-us'] = 'Block calls from a number. Either select a number from the list above or enter the number, name and enable below.';
+	
+	$text['label-edit-note']['en-us'] = 'Block calls from a number. Edit the name and enable/disable below.';
+	
+	$text['label-exact-number']['en-us'] = 'Enter the exact number.';
+	
+	$text['label-reject']['en-us'] = 'Reject';
+	
+	$text['label-busy']['en-us'] = 'Busy';
+	
+	$text['label-action-message']['en-us'] = 'Set an action for calls from this number.';
+	
+	$text['label-true']['en-us'] = 'true';
+	
+	$text['label-false']['en-us'] = 'false';
+	
+	$text['label-enable-message']['en-us'] = 'Set to true to enable call blocking for this number.';
+	
+	$text['button-save']['en-us'] = 'Save';
+	
+	$text['button-back']['en-us'] = 'Back';
+
+?>
