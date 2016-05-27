3fef343dc4e5b4a9133dac4df736b68bd0239c38
Finn <finn@finn.io>
2016-05-26 18:25:55 -0700

    Number has a b in it (#14)
    * Number has a b in it
    
    * Apparently i can't spell either
    
    * One more numer i missed

--- sms/sms_mdr.php
+++ sms/sms_mdr.php
@@ -52,7 +52,7 @@ else {
 
 	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
 
-	$sql = "select domain_name, extension, sms_message_uuid,start_stamp,from_numer,to_number,message,direction from v_sms_messages, v_domains, v_extensions where v_sms_messages.domain_uuid = v_domains.domain_uuid and v_sms_messages.extension_uuid = v_extensions.extension_uuid and v_domains.domain_uuid = '" . $domain_uuid . "' order by start_stamp";
+	$sql = "select domain_name, extension, sms_message_uuid,start_stamp,from_number,to_number,message,direction from v_sms_messages, v_domains, v_extensions where v_sms_messages.domain_uuid = v_domains.domain_uuid and v_sms_messages.extension_uuid = v_extensions.extension_uuid and v_domains.domain_uuid = '" . $domain_uuid . "' order by start_stamp";
 	error_log("SQL: " . print_r($sql,true));
 	$prep_statement = $db->prepare(check_sql($sql));
 	$prep_statement->execute();
@@ -124,7 +124,7 @@ else {
 			}
 			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['extension']."&nbsp;</td>\n";
 			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['start_stamp']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['from_numer']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['from_number']."&nbsp;</td>\n";
 			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['to_number']."&nbsp;</td>\n";
 			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['message']."&nbsp;</td>\n";
 			echo "</tr>\n";
@@ -132,7 +132,7 @@ else {
 		} // end foreach
 		unset($sql, $result, $row_count);
 	} // end if
-	
+
 	echo "</table>\n";
 	echo "</form>\n";
 
