0c0f3749c34d27951291ac6718ee5c207f1b4e40
Len <len.pgh@gmail.com>
2016-08-18 00:50:51 -0400

    Update code

--- webrtc/webrtc.php
+++ webrtc/webrtc.php
@@ -2,40 +2,43 @@
 /*
 	FusionPBX
 	Version: MPL 1.1
+
 	The contents of this file are subject to the Mozilla Public License Version
 	1.1 (the "License"); you may not use this file except in compliance with
 	the License. You may obtain a copy of the License at
 	http://www.mozilla.org/MPL/
+
 	Software distributed under the License is distributed on an "AS IS" basis,
 	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 	for the specific language governing rights and limitations under the
 	License.
+
 	The Original Code is FusionPBX
+
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2016
+	Portions created by the Initial Developer are Copyright (C) 2008-2012
 	the Initial Developer. All Rights Reserved.
+
 	Contributor(s):
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Len Graham <Len.pgh@gmail.com>
+	James Rose <james.o.rose@gmail.com>
 */
-//includes
-	require_once "root.php";
-	require_once "resources/require.php";
-
-//check permissions
-	require_once "resources/check_auth.php";
-	if (permission_exists('access_control_view')) {
-		//access granted
-	}
-	else {
-		echo "access denied";
-		exit;
-	}
-
+require_once "root.php";
+require_once "resources/require.php";
+require_once "resources/check_auth.php";
+if (permission_exists('acl_view')) {
+	//access granted
+}
+else {
+	echo "access denied";
+	exit;
+}
 //add multi-lingual support
-	$language = new text;
-	$text = $language->get();
+	require_once "app_languages.php";
+	foreach($text as $key => $value) {
+		$text[$key] = $value[$_SESSION['domain']['language']['code']];
+	}
 
 //get variables used to control the order
 	$order_by = $_GET["order_by"];
@@ -43,31 +46,121 @@
 
 //additional includes
 	require_once "resources/header.php";
+	$documentu['title'] = $text['title-acl'];
 	require_once "resources/paging.php";
 
-//prepare to page the results
-	$sql = "select count(*) as num_rows from v_webrtc ";
-	if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
-	$prep_statement = $db->prepare($sql);
-	if ($prep_statement) {
+//show the content
+	echo "<table width='100%' cellpadding='0' cellspacing='0' border='0'>\n";
+	echo "	<tr>\n";
+	echo "		<td width='50%' align='left' nowrap='nowrap'><b>".$text['header-access_control_list']."</b></td>\n";
+	echo "		<td width='50%' align='right'>&nbsp;</td>\n";
+	echo "	</tr>\n";
+	echo "	<tr>\n";
+	echo "		<td align='left' colspan='2'>\n";
+	echo "			".$text['description-acl']."<br /><br />\n";
+	echo "		</td>\n";
+	echo "	</tr>\n";
+	echo "</table>\n";
+
+	//prepare to page the results
+		$sql = "select count(*) as num_rows from v_acl ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
+		$prep_statement = $db->prepare($sql);
+		if ($prep_statement) {
 		$prep_statement->execute();
-		$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
-		if ($row['num_rows'] > 0) {
+			$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+			if ($row['num_rows'] > 0) {
 				$num_rows = $row['num_rows'];
-		}
-		else {
+			}
+			else {
 				$num_rows = '0';
+			}
 		}
+
+	//prepare to page the results
+		$rows_per_page = 10;
+		$param = "";
+		$page = $_GET['page'];
+		if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
+		list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
+		$offset = $rows_per_page * $page;
+
+	//get the list
+		$sql = "select * from v_acl ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
+		$sql .= "limit $rows_per_page offset $offset ";
+		$prep_statement = $db->prepare(check_sql($sql));
+		$prep_statement->execute();
+		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+		$result_count = count($result);
+		unset ($prep_statement, $sql);
+
+	$c = 0;
+	$row_style["0"] = "row_style0";
+	$row_style["1"] = "row_style1";
+
+	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+	echo "<tr>\n";
+	echo th_order_by('acl_name', $text['label-acl_name'], $order_by, $order);
+	echo th_order_by('acl_type', $text['label-acl_type'], $order_by, $order);
+	echo th_order_by('acl_description', $text['label-acl_description'], $order_by, $order);
+	echo "<td class='list_control_icons'>";
+	if (permission_exists('acl_add')) {
+		echo "<a href='acl_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
 	}
+	else {
+		echo "&nbsp;\n";
+	}
+	echo "</td>\n";
+	echo "<tr>\n";
 
-//prepare to page the results
-	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
-	$param = "";
-	$page = $_GET['page'];
-	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
-	list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
-	$offset = $rows_per_page * $page;
+	if ($result_count > 0) {
+		foreach($result as $row) {
+			if (permission_exists('acl_edit')) {
+				$tr_link = "href='acl_edit.php?id=".$row['acl_uuid']."'";
+			}
+			echo "<tr ".$tr_link.">\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_name']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_type']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_description']."&nbsp;</td>\n";
+			echo "	<td class='list_control_icons'>";
+			if (permission_exists('acl_edit')) {
+				echo "<a href='acl_edit.php?id=".$row['acl_uuid']."' alt='".$text['button-edit']."'>$v_link_label_edit</a>";
+			}
+			if (permission_exists('acl_delete')) {
+				echo "<a href='acl_delete.php?id=".$row['acl_uuid']."' alt='".$text['button-delete']."' onclick=\"return confirm('".$text['confirm-delete']."')\">$v_link_label_delete</a>";
+			}
+			echo "	</td>\n";
+			echo "</tr>\n";
+			if ($c==0) { $c=1; } else { $c=0; }
+		} //end foreach
+		unset($sql, $result, $row_count);
+	} //end if results
 
+	echo "<tr>\n";
+	echo "<td colspan='4' align='left'>\n";
+	echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
+	echo "	<tr>\n";
+	echo "		<td width='33.3%' nowrap='nowrap'>&nbsp;</td>\n";
+	echo "		<td width='33.3%' align='center' nowrap='nowrap'>$paging_controls</td>\n";
+	echo "		<td class='list_control_icons'>";
+	if (permission_exists('acl_add')) {
+		echo 		"<a href='acl_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
+	}
+	else {
+		echo 		"&nbsp;";
+	}
+	echo "		</td>\n";
+	echo "	</tr>\n";
+ 	echo "	</table>\n";
+	echo "</td>\n";
+	echo "</tr>\n";
 
-	Place content here
+	echo "</table>";
+	echo "<br /><br />";
+
+//include the footer
+	require_once "resources/footer.php";
 ?>
