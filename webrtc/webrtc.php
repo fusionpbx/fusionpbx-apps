faf5feb6efe9bbac5f7f8b4d359697c6e6b64b57
Len <Len-PGH@users.noreply.github.com>
2016-08-18 01:13:30 -0400

    Update webrtc.php

--- webrtc/webrtc.php
+++ webrtc/webrtc.php
@@ -22,23 +22,28 @@
 
 	Contributor(s):
 	Mark J Crane <markjcrane@fusionpbx.com>
-	James Rose <james.o.rose@gmail.com>
+	Giovanni Maruzzelli <gmaruzz@opentelecom.it>
+	Len Graham <Len.PGH@gmail.com>
 */
-require_once "root.php";
-require_once "resources/require.php";
-require_once "resources/check_auth.php";
-if (permission_exists('acl_view')) {
-	//access granted
-}
-else {
-	echo "access denied";
-	exit;
-}
-//add multi-lingual support
-	require_once "app_languages.php";
-	foreach($text as $key => $value) {
-		$text[$key] = $value[$_SESSION['domain']['language']['code']];
+
+	<?php
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+
+//check permissions
+	require_once "resources/check_auth.php";
+	if (permission_exists('access_control_view')) {
+		//access granted
 	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
 
 //get variables used to control the order
 	$order_by = $_GET["order_by"];
@@ -46,121 +51,90 @@ else {
 
 //additional includes
 	require_once "resources/header.php";
-	$documentu['title'] = $text['title-acl'];
 	require_once "resources/paging.php";
 
-//show the content
-	echo "<table width='100%' cellpadding='0' cellspacing='0' border='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='50%' align='left' nowrap='nowrap'><b>".$text['header-access_control_list']."</b></td>\n";
-	echo "		<td width='50%' align='right'>&nbsp;</td>\n";
-	echo "	</tr>\n";
-	echo "	<tr>\n";
-	echo "		<td align='left' colspan='2'>\n";
-	echo "			".$text['description-acl']."<br /><br />\n";
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "</table>\n";
-
-	//prepare to page the results
-		$sql = "select count(*) as num_rows from v_acl ";
-		$sql .= "where domain_uuid = '$domain_uuid' ";
-		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
-		$prep_statement = $db->prepare($sql);
-		if ($prep_statement) {
+//prepare to page the results
+	$sql = "select count(*) as num_rows from v_webrtc ";
+	if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
+	$prep_statement = $db->prepare($sql);
+	if ($prep_statement) {
 		$prep_statement->execute();
-			$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
-			if ($row['num_rows'] > 0) {
+		$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+		if ($row['num_rows'] > 0) {
 				$num_rows = $row['num_rows'];
-			}
-			else {
-				$num_rows = '0';
-			}
 		}
-
-	//prepare to page the results
-		$rows_per_page = 10;
-		$param = "";
-		$page = $_GET['page'];
-		if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
-		list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
-		$offset = $rows_per_page * $page;
-
-	//get the list
-		$sql = "select * from v_acl ";
-		$sql .= "where domain_uuid = '$domain_uuid' ";
-		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
-		$sql .= "limit $rows_per_page offset $offset ";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-		$result_count = count($result);
-		unset ($prep_statement, $sql);
-
-	$c = 0;
-	$row_style["0"] = "row_style0";
-	$row_style["1"] = "row_style1";
-
-	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-	echo "<tr>\n";
-	echo th_order_by('acl_name', $text['label-acl_name'], $order_by, $order);
-	echo th_order_by('acl_type', $text['label-acl_type'], $order_by, $order);
-	echo th_order_by('acl_description', $text['label-acl_description'], $order_by, $order);
-	echo "<td class='list_control_icons'>";
-	if (permission_exists('acl_add')) {
-		echo "<a href='acl_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
+		else {
+				$num_rows = '0';
+		}
 	}
-	else {
-		echo "&nbsp;\n";
-	}
-	echo "</td>\n";
-	echo "<tr>\n";
 
-	if ($result_count > 0) {
-		foreach($result as $row) {
-			if (permission_exists('acl_edit')) {
-				$tr_link = "href='acl_edit.php?id=".$row['acl_uuid']."'";
-			}
-			echo "<tr ".$tr_link.">\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_name']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_type']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['acl_description']."&nbsp;</td>\n";
-			echo "	<td class='list_control_icons'>";
-			if (permission_exists('acl_edit')) {
-				echo "<a href='acl_edit.php?id=".$row['acl_uuid']."' alt='".$text['button-edit']."'>$v_link_label_edit</a>";
-			}
-			if (permission_exists('acl_delete')) {
-				echo "<a href='acl_delete.php?id=".$row['acl_uuid']."' alt='".$text['button-delete']."' onclick=\"return confirm('".$text['confirm-delete']."')\">$v_link_label_delete</a>";
-			}
-			echo "	</td>\n";
-			echo "</tr>\n";
-			if ($c==0) { $c=1; } else { $c=0; }
-		} //end foreach
-		unset($sql, $result, $row_count);
-	} //end if results
+//prepare to page the results
+	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
+	$param = "";
+	$page = $_GET['page'];
+	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
+	list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
+	$offset = $rows_per_page * $page;
 
-	echo "<tr>\n";
-	echo "<td colspan='4' align='left'>\n";
-	echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='33.3%' nowrap='nowrap'>&nbsp;</td>\n";
-	echo "		<td width='33.3%' align='center' nowrap='nowrap'>$paging_controls</td>\n";
-	echo "		<td class='list_control_icons'>";
-	if (permission_exists('acl_add')) {
-		echo 		"<a href='acl_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
-	}
-	else {
-		echo 		"&nbsp;";
-	}
-	echo "		</td>\n";
-	echo "	</tr>\n";
- 	echo "	</table>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
-	echo "</table>";
-	echo "<br /><br />";
-
+echo "<!DOCTYPE html> <html lang=\"en\">\n"; 
+echo " <head>\n"; 
+echo "  <meta charset=\"utf-8\">\n"; 
+echo "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"; 
+echo "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"; 
+echo "  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->\n"; 
+echo "  <meta name=\"description\" content=\"A WebRTC client for Verto FreeSWITCH module\">\n"; 
+echo "  <meta name=\"author\" content=\"Giovanni Maruzzelli\">\n"; 
+echo "  <link rel=\"icon\" href=\"favicon.ico\">\n"; 
+echo "  <title>WebRTC Space</title>\n"; 
+echo "  <!-- Bootstrap core CSS -->\n"; 
+//echo "  <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">\n"; 
+echo "  <!-- Custom styles for this template -->\n"; 
+//echo "  <link href=\"high.css\" rel=\"stylesheet\">\n"; 
+echo " </head>\n"; 
+echo " <body>\n"; 
+echo "  <div id=\"conference\">\n"; 
+echo "   <input type=\"hidden\" id=\"hostName\" value=\"verto.len.pw\"/>\n"; 
+echo "   <input type=\"hidden\" id=\"wsURL\" value=\"wss://verto.len.pw:8082\"/>\n"; 
+echo "   <input type=\"hidden\" id=\"login\" value=\"1008\"/>\n"; 
+echo "   <input type=\"hidden\" id=\"passwd\" value=\"auth1234\"/>\n"; 
+echo "   <input type=\"hidden\" id=\"cidnumber\" value=\"WebRTC\"/>\n"; 
+echo "   <div class=\"form-signin\">\n"; 
+echo "    <h2 class=\"form-signin-heading\">WebRTC Space</h2>\n"; 
+echo "    <div id=\"content\" class=\"form-signin-content\">\n"; 
+echo "     <input type=number id=\"ext\" min=8000 max=8999 step=1 class=\"form-control\"\n"; 
+echo "	placeholder=\"Conference Room's Number ? (eg: 8000)\" required autofocus>\n"; 
+echo "     <button class=\"btn btn-lg btn-primary btn-success\" data-inline=\"true\"\n"; 
+echo "	id=\"extbtn\">Choose Room</button>\n"; 
+echo "     <input type=\"text\" id=\"cidname\" class=\"form-control\"\n"; 
+echo "	placeholder=\"insert here your NAME (eg: Squidward)\" required autofocus>\n"; 
+echo "     <button class=\"btn btn-lg btn-primary btn-success\" data-inline=\"true\"\n"; 
+echo "	id=\"callbtn\">Call Conference</button>\n"; 
+echo "     <button class=\"btn btn-lg btn-primary btn-danger\" data-inline=\"true\"\n"; 
+echo "	id=\"backbtn\">Back</button>\n"; 
+echo "    </div>\n"; 
+echo "    <div id=\"video1\" align=\"center\" class=\"embed-responsive embed-responsive-4by3\">\n"; 
+echo "     <video id=\"webcam\" autoplay=\"autoplay\" class=\"embed-responsive-item\"> </video>\n"; 
+echo "    </div>\n"; 
+echo "    <button class=\"btn btn-lg btn-primary btn-danger\" data-inline=\"true\"\n"; 
+echo "	id=\"hupbtn\">Hangup</button>\n"; 
+echo "    <br id=\"br\"/>\n"; 
+echo "    <textarea id=\"chatwin\" class=\"form-control\" rows=\"5\" readonly></textarea>\n"; 
+echo "    <br id=\"br\"/>\n"; 
+echo "    <textarea id=\"chatmsg\" class=\"form-control\" rows=\"1\"\n"; 
+echo "	placeholder=\"type here your chat msg\" autofocus></textarea>\n"; 
+echo "    <button class=\"btn btn-primary btn-success\" data-inline=\"true\"\n"; 
+echo "	id=\"chatsend\">Send Msg</button>\n"; 
+echo "   </div>\n"; 
+echo "   <div align=\"center\" class=\"inner\">\n"; 
+echo "    <p>2016<br/>Techlacom.com</p>\n"; 
+echo "   </div>\n"; 
+echo "  </div>\n"; 
+echo "  <script type=\"text/javascript\" src=\"js/jquery.min.js\"></script>\n"; 
+echo "  <script type=\"text/javascript\" src=\"js/jquery.json-2.4.min.js\"></script>\n"; 
+echo "  <script type=\"text/javascript\" src=\"js/verto-min.js\"></script>\n"; 
+echo "  <script type=\"text/javascript\" src=\"high.js\"></script>\n"; 
+echo " </body> </html>\n"; 
+echo "\n";
 //include the footer
 	require_once "resources/footer.php";
 ?>
