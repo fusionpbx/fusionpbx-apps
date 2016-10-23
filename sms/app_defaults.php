e15cbdcb964e897c2c813a208430b7c2d65504a4
FusionPBX <markjcrane@gmail.com>
2016-10-23 00:34:25 -0600

    Update app_defaults.php

--- sms/app_defaults.php
+++ sms/app_defaults.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	Portions created by the Initial Developer are Copyright (C) 2016
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -25,8 +25,9 @@
 */
 
 if ($domains_processed == 1) {
-
+	
 	$x = 0;
+	$array[$x]['default_setting_uuid'] = '67511e2d-35e1-4f70-80ac-4265ec39d2fe';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'carriers';
 	$array[$x]['default_setting_name'] = 'array';
@@ -34,6 +35,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'true';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '22bcf947-b634-4849-aaec-dd0635ca6f16';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'carriers';
 	$array[$x]['default_setting_name'] = 'array';
@@ -41,6 +43,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'true';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '27b5c0a4-b824-4e51-aadd-9a19023202cc';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'carriers';
 	$array[$x]['default_setting_name'] = 'array';
@@ -48,6 +51,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'true';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '5c8cea06-edb7-4092-bbdd-7fdc89f02eb0';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'carriers';
 	$array[$x]['default_setting_name'] = 'array';
@@ -55,7 +59,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'true';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
-
+	$array[$x]['default_setting_uuid'] = 'e997203c-ca48-45b4-828d-e347ff66fa7c';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'twilio_api_url';
 	$array[$x]['default_setting_name'] = 'text';
@@ -63,6 +67,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '957b31ab-bc8e-4bff-8366-59f17e658550';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'plivo_api_url';
 	$array[$x]['default_setting_name'] = 'text';
@@ -70,6 +75,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = 'ed6cd4a7-9f89-4156-9591-313b9a73bcfa';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'flowroute_api_url';
 	$array[$x]['default_setting_name'] = 'text';
@@ -77,6 +83,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '535e7884-d8af-4c61-967c-cc2a2ebfb6a3';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'teli_api_url';
 	$array[$x]['default_setting_name'] = 'text';
@@ -85,14 +92,16 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_description'] = '';
 	$x++;
 
-
-/*	$array[$x]['default_setting_category'] = 'sms';
+/*
+	$array[$x]['default_setting_uuid'] = '14101c26-c3f9-46aa-a67a-3642752e56f4';
+	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'flowroute_access_key';
 	$array[$x]['default_setting_name'] = 'text';
 	$array[$x]['default_setting_value'] = '';
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '6fd7b4b0-ed90-4aea-9666-919e1b9b4e35';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'flowroute_secret_key';
 	$array[$x]['default_setting_name'] = 'text';
@@ -101,6 +110,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_description'] = '';
 	$x++;
 
+	$array[$x]['default_setting_uuid'] = '551b3948-8328-42be-a873-9a32f8b49463';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'teli_access_key';
 	$array[$x]['default_setting_name'] = 'text';
@@ -108,6 +118,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = 'c7607430-1b55-41ff-934e-7f9142b29df0';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'teli_secret_key';
 	$array[$x]['default_setting_name'] = 'text';
@@ -116,6 +127,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_description'] = '';
 	$x++;
 
+	$array[$x]['default_setting_uuid'] = 'c56042fc-4cd4-425b-a39a-9297aaed7743';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'twilio_access_key';
 	$array[$x]['default_setting_name'] = 'text';
@@ -123,6 +135,7 @@ if ($domains_processed == 1) {
 	$array[$x]['default_setting_enabled'] = 'false';
 	$array[$x]['default_setting_description'] = '';
 	$x++;
+	$array[$x]['default_setting_uuid'] = '784196cb-d012-4a74-8183-4fc0b738c06a';
 	$array[$x]['default_setting_category'] = 'sms';
 	$array[$x]['default_setting_subcategory'] = 'twilio_secret_key';
 	$array[$x]['default_setting_name'] = 'text';
@@ -134,6 +147,7 @@ if ($domains_processed == 1) {
 		
 	//get an array of the default settings
 		$sql = "select * from v_default_settings ";
+		$sql .= "where default_setting_category = 'sms' ";
 		$prep_statement = $db->prepare($sql);
 		$prep_statement->execute();
 		$default_settings = $prep_statement->fetchAll(PDO::FETCH_NAMED);
@@ -153,17 +167,13 @@ if ($domains_processed == 1) {
 			}
 			$x++;
 		}
+		unset($array);
 
-	//add the missing default settings
-		if (count($missing) > 0) foreach ($missing as $row) {
-			//add the default settings
-			$orm = new orm;
-			$orm->name('default_settings');
-			$orm->save($row);
-			$message = $orm->message;
-			unset($orm);
+	//update the array structure
+		if (is_array($missing)) {
+			$array['default_settings'] = $missing;
+			unset($missing);
 		}
-		unset($missing);
 
 }
 
