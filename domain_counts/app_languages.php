31cf7b3ba2858b07f0e35ae238da44772499ac47
konradSC <konradSC@users.noreply.github.com>
2016-12-23 04:26:35 -0500

    Domain Counts (#36)
    * Create app_config.php
    
    * Create app_languages.php
    
    * Create app_menu.php
    
    * Create domain_counts.php
    
    * Create root.php

---
+++ domain_counts/app_languages.php
@@ -0,0 +1,31 @@
+<?php
+$text['title-domain_counts']['en-us'] = "Domain Counts";
+
+$text['header-domain_counts']['en-us'] = "Domain Counts";
+
+$text['description-domain_counts']['en-us'] = "Displays the number of items from all domains";
+
+$text['label-domain_name']['en-us'] = "Domain Name";
+
+$text['label-extensions']['en-us'] = "Extensions";
+
+$text['label-users']['en-us'] = "Users";
+
+$text['label-devices']['en-us'] = "Devices";
+
+$text['label-destinations']['en-us'] = "Destinations";
+
+$text['label-faxes']['en-us'] = "Faxes";
+
+$text['label-ivrs']['en-us'] = "IVR's";
+
+$text['label-voicemails']['en-us'] = "Voicemails";
+
+$text['label-ring_groups']['en-us'] = "Ring Groups";
+
+$text['label-cc_queues']['en-us'] = "CC Queues";
+
+$text['label-contacts']['en-us'] = "Contacts";
+
+$text['button-export']['en-us'] = "Export";
+?>
