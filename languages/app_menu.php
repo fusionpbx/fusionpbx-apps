e647238af20c16028acb9f50c1e7570ede47113a
Matthew Vale <github@mafoo.org>
2017-05-10 12:42:29 +0100

    Added organizer support
    updated to newer root.php
    fixes for flag locations
    fixes for getting the list of languages
    this version was used to produce - https://github.com/fusionpbx/fusionpbx/pull/2572
    ran ourselves though organizer

--- languages/app_menu.php
+++ languages/app_menu.php
@@ -1,17 +1,20 @@
 <?php
 
 $apps[$x]['menu'][0]['title']['en-us'] = "Languages";
-$apps[$x]['menu'][0]['title']['es-cl'] = "";
-$apps[$x]['menu'][0]['title']['fr-fr'] = "";
-$apps[$x]['menu'][0]['title']['pt-pt'] = "";
-$apps[$x]['menu'][0]['title']['pt-br'] = "";
-$apps[$x]['menu'][0]['title']['pl'] = "";
-$apps[$x]['menu'][0]['title']['sv-se'] = "";
-$apps[$x]['menu'][0]['title']['uk'] = "";
-$apps[$x]['menu'][0]['title']['ro'] = "";
-$apps[$x]['menu'][0]['title']['de-at'] = "";
-$apps[$x]['menu'][0]['title']['ar-eg'] = "";
-$apps[$x]['menu'][0]['title']['he'] = "";
+$apps[$x]['menu'][0]['title']['ar-eg'] = "Sprachen"; //Google translate
+$apps[$x]['menu'][0]['title']['de-at'] = "Sprachen"; //Google translate
+$apps[$x]['menu'][0]['title']['de-de'] = "Sprachen"; //Google translate
+$apps[$x]['menu'][0]['title']['es-cl'] = "Idiomas"; //Google translate
+$apps[$x]['menu'][0]['title']['fr-fr'] = "Les langues"; //Google translate
+$apps[$x]['menu'][0]['title']['he-il'] = "שפות"; //Google translate
+$apps[$x]['menu'][0]['title']['nl-nl'] = "Talen"; //Google translate
+$apps[$x]['menu'][0]['title']['pl-pl'] = "Języki"; //Google translate
+$apps[$x]['menu'][0]['title']['pt-br'] = "línguas"; //Google translate
+$apps[$x]['menu'][0]['title']['pt-pt'] = "línguas"; //Google translate
+$apps[$x]['menu'][0]['title']['ru-ru'] = "Языки"; //Google translate
+$apps[$x]['menu'][0]['title']['sv-se'] = "språk"; //Google translate
+$apps[$x]['menu'][0]['title']['uk-ua'] = "мови"; //Google translate
+
 $apps[$x]['menu'][0]['uuid'] = "13a8fd28-6718-4ac1-8acc-0bcffbce29c3";
 $apps[$x]['menu'][0]['parent_uuid'] = "0438b504-8613-7887-c420-c837ffb20cb1";
 $apps[$x]['menu'][0]['category'] = "internal";
--- languages/languages_compare.php
+++ languages/languages_compare.php
@@ -45,18 +45,20 @@ require_once "resources/require.php";
 			$reference_language = check_str($_POST["reference_language"]);
 			$target_language = check_str($_POST["target_language"]);
 			$app_target = check_str($_POST["app_target"]);
+			$organize_app = check_str($_POST["organize_app"]);
+			$organize_all = check_str($_POST["organize_all"]);
 	}
 
 //collect languages
 	$language = new text;
-	
+
 	$language_text = $language->get('all', $app_target, true);
 	foreach ($language_text as $lang_label => $lang_codes) {
 		$language_labels[] = $lang_label;
 		$reference_text[$lang_label] = $lang_codes[$reference_language];
 		$target_text[$lang_label] = $lang_codes[$target_language];
 	}
-	sort($language_labels);
+	asort($language_labels);
 	
 	if($app_target != 'resources'){
 		$global_text = $language->get($reference_language, 'resources', true);
@@ -65,6 +67,19 @@ require_once "resources/require.php";
 
 //add multi-lingual support
 	$text = $language->get();
+
+	if($organize_app and strlen($app_target) > 0) {
+		$language->organize_language($app_target, false);
+		messages::add("Updated $app_target's app_languages.php");
+	}
+	if($organize_all) {
+		$files = glob($_SERVER["DOCUMENT_ROOT"] . PROJECT_PATH . "/*/*/app_languages.php");
+		foreach($files as $file) {
+			$file = preg_replace('/\A.*(\/.*\/.*)\z/', '$1', dirname($file));
+			$language->organize_language($file, true);
+		}
+		messages::add("Updated All app_languages.php's");
+	}
 
 //get the list of installed apps from the core and mod directories
 	$config_list = glob($_SERVER["DOCUMENT_ROOT"] . PROJECT_PATH . "/*/*/app_config.php");
@@ -114,7 +129,7 @@ require_once "resources/require.php";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
 	echo "<fieldset class='container'>";
-	foreach($_SESSION['app']['languages'] as $lang_code){
+	foreach($language->languages as $lang_code){
 		echo "<fieldset class='container'>";
 		echo "	<label class='radio' style='width:200px;'>";
 		echo "<input type='radio' name='reference_language' value='$lang_code' id='reference_language_$lang_code'";
@@ -123,7 +138,7 @@ require_once "resources/require.php";
 			echo " checked='checked'";
 		}
 		echo "/>";
-		echo "<img src='$project_path/themes/flags/$lang_code.png' alt='$lang_code'/>&nbsp;".$text["language-$lang_code"];
+		echo "<img src='".PROJECT_PATH."/core/install/resources/images/flags/$lang_code.png' alt='$lang_code'/>&nbsp;".$text["language-$lang_code"];
 		echo "</label>\n";
 		echo "</fieldset>";
 	}
@@ -138,7 +153,7 @@ require_once "resources/require.php";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
 	echo "<fieldset class='container'>";
-	foreach($_SESSION['app']['languages'] as $lang_code){
+	foreach($language->languages as $lang_code){
 		echo "<fieldset class='container'>";
 		echo "	<label class='radio' style='width:200px;'>";
 		echo "<input type='radio' name='target_language' value='$lang_code' id='target_language_$lang_code'";
@@ -147,7 +162,7 @@ require_once "resources/require.php";
 			echo " checked='checked'";
 		}
 		echo "/>";
-		echo "<img src='$project_path/themes/flags/$lang_code.png' alt='$lang_code'/>&nbsp;".$text["language-$lang_code"];
+		echo "<img src='".PROJECT_PATH."/core/install/resources/images/flags/$lang_code.png' alt='$lang_code'/>&nbsp;".$text["language-$lang_code"];
 		echo "</label>\n";
 		echo "</fieldset>";
 	}
@@ -178,6 +193,8 @@ require_once "resources/require.php";
 	echo "	<div style='text-align:right'>\n";
 	echo "    <button type='button' onclick=\"history.go(-1);\">".$text['button-back']."</button>\n";
 	echo "    <button type='submit' id='next'>".$text['button-search']."</button>\n";
+	echo "    <button type='submit' id='organize_app' name='organize_app' value='1'>Organize Application's language</button>\n";
+	echo "    <button type='submit' id='organize_all' name='organize_all' value='1'>Organize All language</button>\n";
 	echo "	</div>\n";
 	echo "</form>\n";
 
@@ -188,16 +205,16 @@ require_once "resources/require.php";
 	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 	echo "<tr>\n";
 	echo "<th>".$text['label-tag']."</th>";
-	echo "<th><img src='$project_path/themes/flags/$reference_language.png' alt='$reference_language'/>&nbsp;".$text["language-$reference_language"]."</th>\n";
-	if(strlen($target_language) > 0 ) { echo "<th><img src='$project_path/themes/flags/$target_language.png' alt='$target_language'/>&nbsp;".$text["language-$target_language"]."</th>\n"; }
+	echo "<th><img src='".PROJECT_PATH."/core/install/resources/images/flags/$reference_language.png' alt='$reference_language'/>&nbsp;".$text["language-$reference_language"]."</th>\n";
+	if(strlen($target_language) > 0) { echo "<th><img src='".PROJECT_PATH."/core/install/resources/images/flags/$target_language.png' alt='$target_language'/>&nbsp;".$text["language-$target_language"]."</th>\n"; }
 	echo "</tr>\n";
 	$language_count = 0;
 	foreach ($language_labels as $lang_label){
-		if( preg_match( '/\Alanguage-\w{2}(?:-\w{2})?\z/', $lang_label) ) { break; }
+		if( preg_match( '/\Alanguage-\w{2}(?:-\w{2})?\z/', $lang_label) ) { continue; }
 		echo "<tr>\n";
 		echo "<td class='vncellreq' valign='top' align='left' nowrap>$lang_label";
 		if(isset($global_text[$lang_label])){
-			echo "&nbsp;<img src='$project_path/themes/enhanced/images/warning.png' alt='!' title=\"".$text['warning-global_already_defined']."'".$global_text[$lang_label]."'\"/>";
+			echo "&nbsp;<img src='$project_path/themes/default/images/warning.png' alt='!' title=\"".$text['warning-global_already_defined']."'".$global_text[$lang_label]."'\"/>";
 		}
 		echo "</td>\n";
 		echo "<td class='vtable' align='left'>";
