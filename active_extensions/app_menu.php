e622cedd8530f8f539fc8db580fd22628a6d180d
FusionPBX <markjcrane@gmail.com>
2018-04-12 21:57:27 -0600

    Create app_menu.php

---
+++ active_extensions/app_menu.php
@@ -0,0 +1,30 @@
+<?php
+
+$y=0;
+$apps[$x]['menu'][$y]['title']['en-us'] = "Active Extensions";
+$apps[$x]['menu'][$y]['title']['ar-eg'] = "الأرقام الداخلية";
+$apps[$x]['menu'][$y]['title']['de-at'] = "Aktive Nebenstellen";
+$apps[$x]['menu'][$y]['title']['de-ch'] = "";
+$apps[$x]['menu'][$y]['title']['de-de'] = "Aktive Nebenstellen";
+$apps[$x]['menu'][$y]['title']['es-cl'] = "Extensiones Activas";
+$apps[$x]['menu'][$y]['title']['es-mx'] = "Extensiones Activas";
+$apps[$x]['menu'][$y]['title']['fr-ca'] = "Post téléphonique";
+$apps[$x]['menu'][$y]['title']['fr-fr'] = "Appels en Extensions";
+$apps[$x]['menu'][$y]['title']['he-il'] = "שלוחות";
+$apps[$x]['menu'][$y]['title']['it-it'] = "Interni";
+$apps[$x]['menu'][$y]['title']['nl-nl'] = "";
+$apps[$x]['menu'][$y]['title']['pl-pl'] = "Aktywne wewnętrzne";
+$apps[$x]['menu'][$y]['title']['pt-br'] = "Extensões Activas";
+$apps[$x]['menu'][$y]['title']['pt-pt'] = "Extensões Activas";
+$apps[$x]['menu'][$y]['title']['ro-ro'] = "Extensii";
+$apps[$x]['menu'][$y]['title']['ru-ru'] = "Внутренние номера";
+$apps[$x]['menu'][$y]['title']['sv-se'] = "Aktiva Anknytningar";
+$apps[$x]['menu'][$y]['title']['uk-ua'] = "Активні Розширення";
+$apps[$x]['menu'][0]['uuid'] = "464af9a7-30ab-4a67-b5ce-81309e1faefa";
+$apps[$x]['menu'][0]['parent_uuid'] = "0438b504-8613-7887-c420-c837ffb20cb1";
+$apps[$x]['menu'][0]['category'] = "internal";
+$apps[$x]['menu'][0]['path'] = "/app/active_extensions/active_extensions.php";
+$apps[$x]['menu'][0]['groups'][] = "superadmin";
+$apps[$x]['menu'][0]['groups'][] = "admin";
+
+?>
