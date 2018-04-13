3a2f5b024a1dfd9d483e04d2776c5dec8d9f0240
FusionPBX <markjcrane@gmail.com>
2018-04-12 21:56:24 -0600

    Create app_languages.php

---
+++ active_extensions/app_languages.php
@@ -0,0 +1,373 @@
+<?php
+
+$text['title-2']['en-us'] = "Active Extensions";
+$text['title-2']['es-cl'] = "Extensiones Activas";
+$text['title-2']['fr-fr'] = "Extensions Actives";
+$text['title-2']['pt-pt'] = "Extensões Ativas";
+$text['title-2']['pt-br'] = "Ramais ativos";
+$text['title-2']['pl'] = "Aktywne numery wewnętrzne";
+$text['title-2']['sv-se'] = "Aktiva anknytningar";
+$text['title-2']['uk'] = "Активні розширення";
+$text['title-2']['de-at'] = "Aktive Nebenstellen";
+$text['title-2']['he'] = "שלוחה פעילה";
+
+$text['title']['en-us'] = "Active Calls";
+$text['title']['es-cl'] = "Llamadas Activas";
+$text['title']['fr-fr'] = "Communications Actives";
+$text['title']['pt-pt'] = "Chamadas Ativas";
+$text['title']['pt-br'] = "Configurações da Conta";
+$text['title']['pl'] = "Aktywne Rozmowy";
+$text['title']['sv-se'] = "Aktiva Samtal";
+$text['title']['uk'] = "Активні дзвінки";
+$text['title']['de-at'] = "Aktive Gespräche";
+$text['title']['he'] = "הגדרת חשבון";
+
+$text['label-time']['en-us'] = "Time";
+$text['label-time']['es-cl'] = "Tiempo";
+$text['label-time']['fr-fr'] = "Temps";
+$text['label-time']['pt-pt'] = "Tempo";
+$text['label-time']['pt-br'] = "Fuso Horário";
+$text['label-time']['pl'] = "Strefa czasowa";
+$text['label-time']['sv-se'] = "Tid";
+$text['label-time']['uk'] = "Час";
+$text['label-time']['de-at'] = "Zeit";
+$text['label-time']['he'] = "אזור זמן";
+
+$text['label-stop']['en-us'] = "stop record";
+$text['label-stop']['es-cl'] = "terminar grabación";
+$text['label-stop']['fr-fr'] = "arrêter l'enregistrement";
+$text['label-stop']['pt-pt'] = "parar de gravar";
+$text['label-stop']['pt-br'] = "Parar";
+$text['label-stop']['pl'] = "zatrzymaj nagrywanie";
+$text['label-stop']['sv-se'] = "Stoppa inspelning";
+$text['label-stop']['uk'] = "Зупинити запис";
+$text['label-stop']['de-at'] = "Stoppe Aufnahme";
+$text['label-stop']['he'] = "הפסק הקלטה";
+
+$text['label-status']['en-us'] = "Status";
+$text['label-status']['es-cl'] = "Estado";
+$text['label-status']['fr-fr'] = "Etat";
+$text['label-status']['pt-pt'] = "Estado";
+$text['label-status']['pt-br'] = "Estado";
+$text['label-status']['pl'] = "Status";
+$text['label-status']['sv-se'] = "Status";
+$text['label-status']['uk'] = "Статус";
+$text['label-status']['de-at'] = "Status";
+$text['label-status']['he'] = "סטטוס";
+
+$text['label-start']['en-us'] = "start record";
+$text['label-start']['es-cl'] = "iniciar grabación";
+$text['label-start']['fr-fr'] = "commencer l'enregistrement";
+$text['label-start']['pt-pt'] = "iniciar a gravação";
+$text['label-start']['pt-br'] = "Iniciar";
+$text['label-start']['pl'] = "rozpocznij nagrywanie";
+$text['label-start']['sv-se'] = "Starta inspelning";
+$text['label-start']['uk'] = "Запустити запис";
+$text['label-start']['de-at'] = "Starte Aufnahme";
+$text['label-start']['he'] = "התחל הקלטה";
+
+$text['label-secure']['en-us'] = "Secure";
+$text['label-secure']['es-cl'] = "Seguro";
+$text['label-secure']['fr-fr'] = "Securisé";
+$text['label-secure']['pt-pt'] = "Seguro";
+$text['label-secure']['pt-br'] = "Seguro";
+$text['label-secure']['pl'] = "Bezpieczny";
+$text['label-secure']['sv-se'] = "Skydda";
+$text['label-secure']['uk'] = "Захищено";
+$text['label-secure']['de-at'] = "Sicher";
+$text['label-secure']['he'] = "מאובטח";
+
+$text['label-refresh_pause']['en-us'] = "Pause Refresh";
+$text['label-refresh_pause']['es-cl'] = "Actualizar Pausa";
+$text['label-refresh_pause']['pt-pt'] = "Pausa Atualizar";
+$text['label-refresh_pause']['fr-fr'] = "Pause Actualiser";
+$text['label-refresh_pause']['pt-br'] = "Pausar atualizaÃ§Ã£o";
+$text['label-refresh_pause']['pl'] = "Pauzuj odÅ›wieÅ¼anie";
+$text['label-refresh_pause']['uk'] = "пауза Оновити";
+$text['label-refresh_pause']['de-at'] = "Seite neu laden deaktivieren";
+$text['label-refresh_pause']['he'] = "עצור רענון";
+
+$text['label-refresh_enable']['en-us'] = "Enable Refresh";
+$text['label-refresh_enable']['es-cl'] = "Activar Actualizar";
+$text['label-refresh_enable']['pt-pt'] = "Habilitar Atualização";
+$text['label-refresh_enable']['fr-fr'] = "Activer Actualiser";
+$text['label-refresh_enable']['pt-br'] = "Habilitar atualizaÃ§Ã£o";
+$text['label-refresh_enable']['pl'] = "WÅ‚Ä…cz odÅ›wieÅ¼anie";
+$text['label-refresh_enable']['uk'] = "включити Оновити";
+$text['label-refresh_enable']['de-at'] = "Seite neu laden aktivieren";
+$text['label-refresh_enable']['he'] = "אפשר רענון";
+
+$text['label-profile']['en-us'] = "Profile";
+$text['label-profile']['es-cl'] = "Perfil";
+$text['label-profile']['fr-fr'] = "Profile";
+$text['label-profile']['pt-pt'] = "Perfil";
+$text['label-profile']['pt-br'] = "Perfil";
+$text['label-profile']['pl'] = "Profil";
+$text['label-profile']['sv-se'] = "Profil";
+$text['label-profile']['uk'] = "Профіль";
+$text['label-profile']['de-at'] = "Profil";
+$text['label-profile']['he'] = "פרופיל";
+
+$text['label-park-extension']['en-us'] = "Park";
+$text['label-park-extension']['es-cl'] = "Parquear Extensión";
+$text['label-park-extension']['fr-fr'] = "parquer";
+$text['label-park-extension']['pt-pt'] = "Parquear Extensão";
+$text['label-park-extension']['pt-br'] = "Extensão estacionamento";
+$text['label-park-extension']['pl'] = "Parkowanie";
+$text['label-park-extension']['sv-se'] = "Parkera";
+$text['label-park-extension']['uk'] = "Паркувати";
+$text['label-park-extension']['de-at'] = "Parken";
+$text['label-park-extension']['he'] = "חניה";
+
+$text['label-park']['en-us'] = "park";
+$text['label-park']['es-cl'] = "parquear";
+$text['label-park']['fr-fr'] = "parquer";
+$text['label-park']['pt-pt'] = "parquear";
+$text['label-park']['pt-br'] = "Estacionamento";
+$text['label-park']['pl'] = "parkowanie";
+$text['label-park']['sv-se'] = "Parkera";
+$text['label-park']['uk'] = "паркувати";
+$text['label-park']['de-at'] = "Parken";
+$text['label-park']['he'] = "חניה";
+
+$text['label-opt']['en-us'] = "Options";
+$text['label-opt']['es-cl'] = "Opciones";
+$text['label-opt']['fr-fr'] = "Options";
+$text['label-opt']['pt-pt'] = "Opções";
+$text['label-opt']['pt-br'] = "Opções";
+$text['label-opt']['pl'] = "Opcje";
+$text['label-opt']['sv-se'] = "Alternativ";
+$text['label-opt']['uk'] = "Опції";
+$text['label-opt']['de-at'] = "Optionen";
+$text['label-opt']['he'] = "אפשרויות";
+
+$text['label-number']['en-us'] = "Number";
+$text['label-number']['es-cl'] = "Número";
+$text['label-number']['fr-fr'] = "Numéro";
+$text['label-number']['pt-pt'] = "Numero";
+$text['label-number']['pt-br'] = "Número";
+$text['label-number']['pl'] = "Numer";
+$text['label-number']['sv-se'] = "Nummer";
+$text['label-number']['uk'] = "Номер";
+$text['label-number']['de-at'] = "Nummer";
+$text['label-number']['he'] = "מספר";
+
+$text['label-message']['en-us'] = "Message";
+$text['label-message']['es-cl'] = "Mensaje";
+$text['label-message']['fr-fr'] = "Message";
+$text['label-message']['pt-pt'] = "Mensagem";
+$text['label-message']['pt-br'] = "Mensagem";
+$text['label-message']['pl'] = "Wiadomość";
+$text['label-message']['sv-se'] = "Meddelande";
+$text['label-message']['uk'] = "Повідомлення";
+$text['label-message']['de-at'] = "Nachricht";
+$text['label-message']['he'] = "הודעה";
+
+$text['label-hangup']['en-us'] = "Hangup";
+$text['label-hangup']['es-cl'] = "Terminar";
+$text['label-hangup']['fr-fr'] = "Raccrocher";
+$text['label-hangup']['pt-pt'] = "Terminar";
+$text['label-hangup']['pt-br'] = "Desligar";
+$text['label-hangup']['pl'] = "Zakończ rozmowę";
+$text['label-hangup']['sv-se'] = "Avsluta";
+$text['label-hangup']['uk'] = "Розірвати";
+$text['label-hangup']['de-at'] = "Auflegen";
+$text['label-hangup']['he'] = "ניתוק";
+
+$text['label-ext']['en-us'] = "Ext";
+$text['label-ext']['es-cl'] = "Extensión";
+$text['label-ext']['fr-fr'] = "extension";
+$text['label-ext']['pt-pt'] = "Extensão";
+$text['label-ext']['pt-br'] = "Extensão";
+$text['label-ext']['pl'] = "Numer wewnętrzny";
+$text['label-ext']['sv-se'] = "Ank";
+$text['label-ext']['uk'] = "Ext ";
+$text['label-ext']['de-at'] = "Nst";
+$text['label-ext']['he'] = "שלוחה";
+
+$text['label-domain']['en-us'] = "Domain";
+$text['label-domain']['es-cl'] = "Dominio";
+$text['label-domain']['pt-pt'] = "Domínio";
+$text['label-domain']['fr-fr'] = "Domaine";
+$text['label-domain']['pt-br'] = "Dominio";
+$text['label-domain']['pl'] = "Domena";
+$text['label-domain']['uk'] = "Домен";
+$text['label-domain']['de-at'] = "Domain";
+$text['label-domain']['he'] = "תחום";
+
+$text['label-destination']['en-us'] = "Dest";
+$text['label-destination']['es-cl'] = "Destino";
+$text['label-destination']['fr-fr'] = "Dest";
+$text['label-destination']['pt-pt'] = "Destino";
+$text['label-destination']['pt-br'] = "Número de destino";
+$text['label-destination']['pl'] = "Destynacja";
+$text['label-destination']['sv-se'] = "Dest";
+$text['label-destination']['uk'] = "Номер";
+$text['label-destination']['de-at'] = "Ziel";
+$text['label-destination']['he'] = "יעד";
+
+$text['label-created']['en-us'] = "Created";
+$text['label-created']['es-cl'] = "Creado";
+$text['label-created']['fr-fr'] = "Créé";
+$text['label-created']['pt-pt'] = "Criado";
+$text['label-created']['pt-br'] = "Criado";
+$text['label-created']['pl'] = "Utworzno";
+$text['label-created']['sv-se'] = "Skapad";
+$text['label-created']['uk'] = "Створено";
+$text['label-created']['de-at'] = "Geöffnet";
+$text['label-created']['he'] = "נוצר";
+
+$text['label-codec']['en-us'] = "Read / Write Codec";
+$text['label-codec']['es-cl'] = "Codec de Lectura / Escritura ";
+$text['label-codec']['fr-fr'] = "décode/encode";
+$text['label-codec']['pt-pt'] = "Codec de Leitura / Escrita ";
+$text['label-codec']['pt-br'] = "Codec da leitura / escrita";
+$text['label-codec']['pl'] = "Kodek read/write";
+$text['label-codec']['sv-se'] = "Läs/Skriv Codec";
+$text['label-codec']['uk'] = "Кодеки читання/запису";
+$text['label-codec']['de-at'] = "Lese / Schreib Codec";
+$text['label-codec']['he'] = "קריאה / כתיבה codec";
+
+$text['label-cid-number']['en-us'] = "CID Number";
+$text['label-cid-number']['es-cl'] = "Número CID";
+$text['label-cid-number']['fr-fr'] = "Numéro Appelant";
+$text['label-cid-number']['pt-pt'] = "Número CID";
+$text['label-cid-number']['pt-br'] = "Número CID";
+$text['label-cid-number']['pl'] = "Prezentacja numeru dzwoniącego";
+$text['label-cid-number']['sv-se'] = "CID, nummerspresentation";
+$text['label-cid-number']['uk'] = "CID Номер";
+$text['label-cid-number']['de-at'] = "Anrufer Nummer";
+$text['label-cid-number']['he'] = "זהוי מספר";
+
+$text['label-cid-name']['en-us'] = "CID Name";
+$text['label-cid-name']['es-cl'] = "Nombre CID";
+$text['label-cid-name']['fr-fr'] = "Nom Appelant";
+$text['label-cid-name']['pt-pt'] = "Nome CID";
+$text['label-cid-name']['pt-br'] = "Nome CID";
+$text['label-cid-name']['pl'] = "Prezentacja nazwy dzwoniącego";
+$text['label-cid-name']['sv-se'] = "CID namnpresentation";
+$text['label-cid-name']['uk'] = "CID Ім’я";
+$text['label-cid-name']['de-at'] = "Anrufer Name";
+$text['label-cid-name']['he'] = "זהוי שם";
+
+$text['label-app']['en-us'] = "Application";
+$text['label-app']['es-cl'] = "Aplicación";
+$text['label-app']['fr-fr'] = "Application";
+$text['label-app']['pt-pt'] = "Aplicação";
+$text['label-app']['pt-br'] = "Aplicação";
+$text['label-app']['pl'] = "Aplikacja";
+$text['label-app']['sv-se'] = "Ansökan";
+$text['label-app']['uk'] = "Додаток";
+$text['label-app']['de-at'] = "Anwendung";
+$text['label-app']['he'] = "אפליקציה";
+
+$text['description-2']['en-us'] = "Use this to view all extensions and monitor and interact with active calls.";
+$text['description-2']['es-cl'] = "Utilice este menú para ver todas las extensiones, monitorear e interactuar con las llamadas activas.";
+$text['description-2']['fr-fr'] = "Voir toutes les extensions, superviser et interagir avec les Communications Actives.";
+$text['description-2']['pt-pt'] = "Utilize este menu para ver todas as extensões, monitorizar e interagir com as chamadas activas.";
+$text['description-2']['pt-br'] = "A informação contem a origem, destino, duração e outros detalhes úteis da chamada.";
+$text['description-2']['pl'] = "Użyj tej opcji, aby zobaczyć wszystkie numery wewnętrzne i monitorować interakcję z aktywnymi rozmowami.";
+$text['description-2']['sv-se'] = "Använd detta för att se alla anknytningar och monitorera och hantera aktiva samtal.";
+$text['description-2']['uk'] = "Використовуйте це, щоб переглянути всі розширення, контролювати і взаємодіяти з активними викликами.";
+$text['description-2']['de-at'] = "Benutzen Sie die Funktionen um alle Nebenstellen anzuzeigen, zu überwachen und aktive Gespräche zu steuern.";
+$text['description-2']['he'] = "";
+
+$text['description']['en-us'] = "Use this to monitor and interact with the active calls.";
+$text['description']['es-cl'] = "Utilice este menú para monitorear e interactuar con las llamadas activas.";
+$text['description']['fr-fr'] = "Superviser et interagir avec les Communications Actives.";
+$text['description']['pt-pt'] = "Utilize este menu para monitorizar e interagir com as chamadas activas.";
+$text['description']['pt-br'] = "Editar informações da conta.";
+$text['description']['pl'] = "Użyć tej opcji do monitorowania i interakcji z aktywnymi rozmowami.";
+$text['description']['sv-se'] = "Använd detta för att monitorera och hantera aktiva samtal.";
+$text['description']['uk'] = "Використовуйте це, щоб контролювати і взаємодіяти з активними викликами.";
+$text['description']['de-at'] = "Benutzen Sie die Funktionen um aktive Gespräche zu steuern.";
+$text['description']['he'] = "ערוך את פרטי החשבון";
+
+$text['confirm-socket']['en-us'] = "Connection to Event Socket failed.";
+$text['confirm-socket']['es-cl'] = "Conexión con socket fallida.";
+$text['confirm-socket']['fr-fr'] = "la Connexion au Event Socket a échoué.";
+$text['confirm-socket']['pt-pt'] = "A conexão ao Event Socket falhou.";
+$text['confirm-socket']['pt-br'] = "A conexão ao evento socket falhou";
+$text['confirm-socket']['pl'] = "Polaczenie do  Event Socket nie powiodło się";
+$text['confirm-socket']['sv-se'] = "Anslutning till 'Event Socket' misslyckades";
+$text['confirm-socket']['uk'] = "Помилка підключення до сокета подій";
+$text['confirm-socket']['de-at'] = "Verbindung zum Event Socket fehlgeschlagen.";
+$text['confirm-socket']['he'] = "";
+
+$text['confirm-hangup']['en-us'] = "Do you really want to hangup this call?";
+$text['confirm-hangup']['es-cl'] = "¿Realmente desea terminar esta llamada?";
+$text['confirm-hangup']['fr-fr'] = "Voulez-vous vraiment raccrocher cette communication?";
+$text['confirm-hangup']['pt-pt'] = "Deseja realmente terminar esta chamada?";
+$text['confirm-hangup']['pt-br'] = "Deseja realmente finalizar essa chamada?";
+$text['confirm-hangup']['pl'] = "Czy na pewno chcesz się rozłączyć tę rozmowę?";
+$text['confirm-hangup']['sv-se'] = "Vill du verkligen avsluta detta samtal";
+$text['confirm-hangup']['uk'] = "Ви дійсно бажаєте розірвати дзвінок?";
+$text['confirm-hangup']['de-at'] = "Wollen Sie dieses Gespräch wirklich beenden?";
+$text['confirm-hangup']['he'] = "";
+
+$text['check-onbreak-status']['en-us'] = "On Break";
+$text['check-onbreak-status']['es-cl'] = "En Pausa";
+$text['check-onbreak-status']['fr-fr'] = "En Pause";
+$text['check-onbreak-status']['pt-pt'] = "Em Pausa";
+$text['check-onbreak-status']['pt-br'] = "Em Pausa";
+$text['check-onbreak-status']['pl'] = "Na przerwie";
+$text['check-onbreak-status']['sv-se'] = "På Rast";
+$text['check-onbreak-status']['uk'] = "На перерві";
+$text['check-onbreak-status']['de-at'] = "Auf Pause";
+$text['check-onbreak-status']['he'] = "בהפסקה";
+
+$text['check-loggedout-status']['en-us'] = "Logged Out";
+$text['check-loggedout-status']['es-cl'] = "Desconectado";
+$text['check-loggedout-status']['fr-fr'] = "Délogué";
+$text['check-loggedout-status']['pt-pt'] = "Desligado";
+$text['check-loggedout-status']['pt-br'] = "Desligado";
+$text['check-loggedout-status']['pl'] = "Wylogowany";
+$text['check-loggedout-status']['sv-se'] = "Utloggad";
+$text['check-loggedout-status']['uk'] = "Вийшов";
+$text['check-loggedout-status']['de-at'] = "Abgemeldet";
+$text['check-loggedout-status']['he'] = "התנתק";
+
+$text['check-do-not-disturb-status']['en-us'] = "Do Not Disturb";
+$text['check-do-not-disturb-status']['es-cl'] = "No molestar";
+$text['check-do-not-disturb-status']['fr-fr'] = "Ne Pas Déranger";
+$text['check-do-not-disturb-status']['pt-pt'] = "Não Perturbar";
+$text['check-do-not-disturb-status']['pt-br'] = "Não Pertube";
+$text['check-do-not-disturb-status']['pl'] = "Nie przeszkadzaj (DND)";
+$text['check-do-not-disturb-status']['sv-se'] = "Stör Ej";
+$text['check-do-not-disturb-status']['uk'] = "Не турбувати";
+$text['check-do-not-disturb-status']['de-at'] = "Bitte nicht Stören";
+$text['check-do-not-disturb-status']['he'] = "לא להפריע";
+
+$text['check-available-status']['en-us'] = "Available";
+$text['check-available-status']['es-cl'] = "Disponible";
+$text['check-available-status']['fr-fr'] = "Disponible";
+$text['check-available-status']['pt-pt'] = "Disponível";
+$text['check-available-status']['pt-br'] = "Disponível";
+$text['check-available-status']['pl'] = "Dostępny";
+$text['check-available-status']['sv-se'] = "Anträffbar";
+$text['check-available-status']['uk'] = "Доступний";
+$text['check-available-status']['de-at'] = "Erreichbar";
+$text['check-available-status']['he'] = "זמין";
+
+$text['check-available-on-demand-status']['en-us'] = "Available (On Demand)";
+$text['check-available-on-demand-status']['es-cl'] = "Disponible (On Demand)";
+$text['check-available-on-demand-status']['fr-fr'] = "Disponible (Sur Demande)";
+$text['check-available-on-demand-status']['pt-pt'] = "Disponível (A pedido)";
+$text['check-available-on-demand-status']['pt-br'] = "Disponível (A pedido)";
+$text['check-available-on-demand-status']['pl'] = "Dostępny (na żądanie)";
+$text['check-available-on-demand-status']['sv-se'] = "Anträffbar (På Begäran) ";
+$text['check-available-on-demand-status']['uk'] = "Доступний (на вимогу)";
+$text['check-available-on-demand-status']['de-at'] = "Erreichbar (Auf Anfrage)";
+$text['check-available-on-demand-status']['he'] = "";
+
+$text['button-show_all']['en-us'] = "Show All";
+$text['button-show_all']['es-cl'] = "Mostrar Todos";
+$text['button-show_all']['pt-pt'] = "Mostrar Todos";
+$text['button-show_all']['fr-fr'] = "Montrer Tout";
+$text['button-show_all']['pt-br'] = "Mostrar Todos";
+$text['button-show_all']['pl'] = "Pokaż Wszystkie";
+$text['button-show_all']['uk'] = "Показати всі";
+$text['button-show_all']['de-at'] = "Alle anzeigen";
+$text['button-show_all']['he'] = "";
+
+?>
