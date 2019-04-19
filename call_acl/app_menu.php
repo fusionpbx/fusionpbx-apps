810127a7a36e36ad2d51520f012e98f05e4700ef
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2019-04-19 16:09:30 +0300

    Call ACL app added

---
+++ call_acl/app_menu.php
@@ -0,0 +1,13 @@
+<?php
+
+	$y=0;
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Call ACL";
+
+	$apps[$x]['menu'][$y]['uuid'] = "3f9bcc10-8d16-4619-a8fa-628a3ef6de30";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+	$apps[$x]['menu'][$y]['category'] = "internal";
+	$apps[$x]['menu'][$y]['path'] = "/app/call_acl/call_acl.php";
+	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+	$apps[$x]['menu'][$y]['groups'][] = "admin";
+
+?>
