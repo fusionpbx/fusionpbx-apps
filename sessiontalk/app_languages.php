41086b103db3ca0dc389d98b67311513e25f05c7
konradSC <konradSC@users.noreply.github.com>
2020-08-31 15:05:17 -0400

    Sessiontalk Cloudphone Provisioning (#147)
    * Sessiontalk Provisioning
    
    Allows for auto provisioning of Sessiontalk's Cloudphone with push.
    
    * Delete apple_app_store.png
    
    * Delete google_play.png
    
    * Create README.rst

---
+++ sessiontalk/app_languages.php
@@ -0,0 +1,66 @@
+<?php
+
+$text['title-sessiontalk']['en-us'] = "Sessiontalk";
+$text['title-sessiontalk']['en-gb'] = "Sessiontalk";
+$text['title-sessiontalk']['ar-eg'] = "";
+$text['title-sessiontalk']['de-at'] = "";
+$text['title-sessiontalk']['de-ch'] = "";
+$text['title-sessiontalk']['de-de'] = "";
+$text['title-sessiontalk']['es-cl'] = "";
+$text['title-sessiontalk']['es-mx'] = "";
+$text['title-sessiontalk']['fr-ca'] = "Sessiontalk";
+$text['title-sessiontalk']['fr-fr'] = "Sessiontalk";
+$text['title-sessiontalk']['he-il'] = "";
+$text['title-sessiontalk']['it-it'] = "";
+$text['title-sessiontalk']['nl-nl'] = "Sessiontalk";
+$text['title-sessiontalk']['pl-pl'] = "";
+$text['title-sessiontalk']['pt-br'] = "";
+$text['title-sessiontalk']['pt-pt'] = "";
+$text['title-sessiontalk']['ro-ro'] = "";
+$text['title-sessiontalk']['ru-ru'] = "";
+$text['title-sessiontalk']['sv-se'] = "";
+$text['title-sessiontalk']['uk-ua'] = "";
+
+$text['title_description-sessiontalk']['en-us'] = "Sessiontalk is a soft phone for smart phones or tablets. It can be configured easily with a QR code provided here. To use it download and install Sessiontalk for your device. Start the application and then go to Account Settings press the + to add a new account. Press on UCM Account (Scan QR Code) and then select the extension and scan the QR code.";
+$text['title_description-sessiontalk']['en-gb'] = "Sessiontalk is a soft phone for smart phones or tablets. It can be configured easily with a QR code provided here. To use it download and install Sessiontalk for your device. Start the application and then go to Account Settings press the + to add a new account. Press on UCM Account (Scan QR Code) and then select the extension and scan the QR code.";
+$text['title_description-sessiontalk']['ar-eg'] = "";
+$text['title_description-sessiontalk']['de-at'] = "";
+$text['title_description-sessiontalk']['de-ch'] = "";
+$text['title_description-sessiontalk']['de-de'] = "";
+$text['title_description-sessiontalk']['es-cl'] = "";
+$text['title_description-sessiontalk']['es-mx'] = "";
+$text['title_description-sessiontalk']['fr-ca'] = "Sessiontalk est un logiciel de téléphonie pour smartphones ou tablettes. Il peut être configuré facilement avec un code QR fourni ici. Pour l'utiliser, téléchargez et installez Sessiontalk sur votre appareil. Lancez l'application, puis allez dans les paramètres du compte, appuyez sur le signe + pour ajouter un nouveau compte. Appuyez sur Compte UCM (Scan QR Code), puis sélectionnez l’extension et scannez le code QR.";
+$text['title_description-sessiontalk']['fr-fr'] = "Sessiontalk est un logiciel de téléphonie pour smartphones ou tablettes. Il peut être configuré facilement avec un code QR fourni ici. Pour l'utiliser, téléchargez et installez Sessiontalk sur votre appareil. Lancez l'application, puis allez dans les paramètres du compte, appuyez sur le signe + pour ajouter un nouveau compte. Appuyez sur Compte UCM (Scan QR Code), puis sélectionnez l’extension et scannez le code QR.";
+$text['title_description-sessiontalk']['he-il'] = "";
+$text['title_description-sessiontalk']['it-it'] = "";
+$text['title_description-sessiontalk']['nl-nl'] = "Sessiontalk is een software telefoon voor 'smart-phones' en 'tablets'. Het kan eenvoudig d.m.v. een QR code voorzien worden van configuratie. Installeer Granstream Wave op je apparaat start de applicatie en ga naar ' Account Settings' en druk op + om een nieuwaccount toe te voegen. Druk op 'UCM account' (Scan QR Code) and kies het toestelnummer en scan de QR code met het apparaat.";
+$text['title_description-sessiontalk']['pl-pl'] = "";
+$text['title_description-sessiontalk']['pt-br'] = "";
+$text['title_description-sessiontalk']['pt-pt'] = "";
+$text['title_description-sessiontalk']['ro-ro'] = "";
+$text['title_description-sessiontalk']['ru-ru'] = "";
+$text['title_description-sessiontalk']['sv-se'] = "";
+$text['title_description-sessiontalk']['uk-ua'] = "";
+
+$text['label-extension']['en-us'] = "Extension";
+$text['label-extension']['en-gb'] = "Extension";
+$text['label-extension']['ar-eg'] = "رقم داخلي";
+$text['label-extension']['de-at'] = "Nebenstelle"; //copied from de-de
+$text['label-extension']['de-ch'] = "Nebenstelle"; //copied from de-de
+$text['label-extension']['de-de'] = "Nebenstelle";
+$text['label-extension']['es-cl'] = "Extensión";
+$text['label-extension']['es-mx'] = "Extensión"; //copied from es-cl
+$text['label-extension']['fr-ca'] = "Extension"; //copied from fr-fr
+$text['label-extension']['fr-fr'] = "Extension";
+$text['label-extension']['he-il'] = "שלוחה";
+$text['label-extension']['it-it'] = "Interno";
+$text['label-extension']['nl-nl'] = "Toestel";
+$text['label-extension']['pl-pl'] = "Numer wewnętrzny";
+$text['label-extension']['pt-br'] = "Ramal";
+$text['label-extension']['pt-pt'] = "Extensão";
+$text['label-extension']['ro-ro'] = "";
+$text['label-extension']['ru-ru'] = "Внутренний Номер";
+$text['label-extension']['sv-se'] = "Anknytning";
+$text['label-extension']['uk-ua'] = "Розширення";
+
+?>
