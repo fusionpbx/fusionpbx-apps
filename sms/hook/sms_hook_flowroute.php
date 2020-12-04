dcc33f3f73f243856e46e20bb08cef37c46e52d0
mbarvoipdrs <71390678+mbarvoipdrs@users.noreply.github.com>
2020-12-04 13:11:13 -0500

    Update sms_hook_flowroute.php
    Update to receive SMS from Flowroute new API format.

--- sms/hook/sms_hook_flowroute.php
+++ sms/hook/sms_hook_flowroute.php
@@ -11,7 +11,8 @@ if (check_acl()) {
 		if ($debug) {
 			error_log('[SMS] REQUEST: ' .  print_r($data, true));
 		}
-		route_and_send_sms($data->from, $data->to, $data->body);	
+	$from = intval(preg_replace('/(^[1])/','', $data->data->attributes->from));
+	route_and_send_sms($from, $data->data->attributes->to, $data->data->attributes->body);	
 	} else {
 	  die("no");
 	}
