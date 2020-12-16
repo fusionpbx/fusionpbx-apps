5eb58386652e4067b089e32453db33f2250a264f
chansizzle <14916599+chansizzle@users.noreply.github.com>
2020-12-16 16:47:31 -0700

    Fix add and update
    fix add and update

--- school_bells/app_languages.php
+++ school_bells/app_languages.php
@@ -3,7 +3,6 @@ $text['title-school_bells']['en-us'] = "School Bells";
 $text['title-school_bells-add']['en-us'] = "Add School Bell";
 $text['title-school_bells-edit']['en-us'] = "Edit School Bell";
 
-
 $text['header-school_bells']['en-us'] = "School Bells";
 
 $text['description-school_bells']['en-us'] = "Scheduled periodic signals using PBX system in cron-like style";
@@ -71,5 +70,49 @@ $text['description-school_bell_timezone']['en-us'] = "Timezone of this schedule"
 $text['description-school_bell_description']['en-us'] = "Description of this schedule";
 
 $text['description-school_bells_schedule_templates']['en-us'] = "System uses cron-type of scheduling. For more information, refer to CRON documentation";
+
+$text['label-add-complete']['en-us'] = "Add Complete";
+$text['label-add-complete']['en-gb'] = "Add Complete";
+$text['label-add-complete']['ar-eg'] = "تمت الأضافة";
+$text['label-add-complete']['de-at'] = "Erfolgreich hinzugefügt"; //copied from de-de
+$text['label-add-complete']['de-ch'] = "Erfolgreich hinzugefügt"; //copied from de-de
+$text['label-add-complete']['de-de'] = "Erfolgreich hinzugefügt";
+$text['label-add-complete']['el-gr'] = "Η προσθήκη ολοκληρώθηκε";
+$text['label-add-complete']['es-cl'] = "Agregar Completo";
+$text['label-add-complete']['es-mx'] = "Agregar Completo"; //copied from es-cl
+$text['label-add-complete']['fr-ca'] = "Ajouté"; //copied from fr-fr
+$text['label-add-complete']['fr-fr'] = "Ajouté";
+$text['label-add-complete']['he-il'] = "הוספה הושלמה";
+$text['label-add-complete']['it-it'] = "Inserimento Completato";
+$text['label-add-complete']['nl-nl'] = "Toevoegen gereed";
+$text['label-add-complete']['pl-pl'] = "Dodano poprawnie";
+$text['label-add-complete']['pt-br'] = "Criação efetuada ";
+$text['label-add-complete']['pt-pt'] = "Adição Efectuada";
+$text['label-add-complete']['ro-ro'] = "Adăugare completă";
+$text['label-add-complete']['ru-ru'] = "Добавление завершено";
+$text['label-add-complete']['sv-se'] = "Tillagd";
+$text['label-add-complete']['uk-ua'] = "Додано";
+
+$text['label-update-complete']['en-us'] = "Update Complete";
+$text['label-update-complete']['en-gb'] = "Update Complete";
+$text['label-update-complete']['ar-eg'] = "اكتمال التحديث";
+$text['label-update-complete']['de-at'] = "Aktualisierung abgeschlossen"; //copied from de-de
+$text['label-update-complete']['de-ch'] = "Aktualisierung abgeschlossen"; //copied from de-de
+$text['label-update-complete']['de-de'] = "Aktualisierung abgeschlossen";
+$text['label-update-complete']['el-gr'] = "Η ενημέρωση ολοκληρώθηκε";
+$text['label-update-complete']['es-cl'] = "Actualización Completa";
+$text['label-update-complete']['es-mx'] = "Actualización Completa"; //copied from es-cl
+$text['label-update-complete']['fr-ca'] = "Mise à jour effectuée"; //copied from fr-fr
+$text['label-update-complete']['fr-fr'] = "Mise à jour effectuée";
+$text['label-update-complete']['he-il'] = "העדכון הושלם";
+$text['label-update-complete']['it-it'] = "Aggiornamento Completato";
+$text['label-update-complete']['nl-nl'] = "Bijgewerkt";
+$text['label-update-complete']['pl-pl'] = "Zaktualizowano poprawnie";
+$text['label-update-complete']['pt-br'] = "Atualização Completa";
+$text['label-update-complete']['pt-pt'] = "Actualização Efectuada";
+$text['label-update-complete']['ro-ro'] = "Actualizare completă";
+$text['label-update-complete']['ru-ru'] = "Обновление завершено";
+$text['label-update-complete']['sv-se'] = "Uppdatering Klar";
+$text['label-update-complete']['uk-ua'] = "Оновлено";
 
 ?>
