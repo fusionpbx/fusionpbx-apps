b71a42fcc4d38c406ef156562ceea9981b1ea417
Alex <40072887+alexdcrane@users.noreply.github.com>
2022-10-11 16:13:49 -0600

    Update school_bells.php

--- school_bells/school_bells.php
+++ school_bells/school_bells.php
@@ -24,6 +24,7 @@
 	Mark J Crane <markjcrane@fusionpbx.com>
 	Igor Olhovskiy <igorolhovskiy@gmail.com>
 */
+
 //set the include path
 	$conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
 	set_include_path(parse_ini_file($conf[0])['document.root']);
