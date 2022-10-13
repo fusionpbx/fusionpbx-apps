f4524cd89b986f46104d3e85bd71cc12e8594ffd
Alex <40072887+alexdcrane@users.noreply.github.com>
2022-10-13 09:26:35 -0600

    Update sms_hook_didww.php

--- sms/hook/sms_hook_didww.php
+++ sms/hook/sms_hook_didww.php
@@ -1,9 +1,12 @@
 <?php
 
-include "../root.php";
+//set the include path
+    $conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
+    set_include_path(parse_ini_file($conf[0])['document.root']);
 
-require_once "resources/require.php";
-require_once "../sms_hook_common.php";
+//includes files
+	require_once "resources/require.php";
+    require_once "../sms_hook_common.php";
 
 error_log(print_r($_POST,true));
 
@@ -19,4 +22,5 @@ $sms_to_did_no=$_POST['to'];
 route_and_send_sms($sms_from, $sms_to_did_no, $sms_text);
 
 die("no");
+
 ?>
