568cf730ab920f393aeb210b9541c628399df37f
Nick <nickb@myfutra.com>
2023-05-05 23:30:15 +0000

    updated requirement for bandwidth response

--- sms/hook/sms_hook_bandwidth.php
+++ sms/hook/sms_hook_bandwidth.php
@@ -19,16 +19,49 @@ if (check_acl()) {
 			$from = $data[0]->message->from;
 			$to = $data[0]->message->owner;
 			$text = $data[0]->message->text;
+			$msg_type = $data[0]->type;
+			$desc = $data[0]->description;
 		} else {
 			$from = $data->message->from;
 			$to = $data->message->owner;
 			$text = $data->message->text;
-		}
+			$msg_type = $data->type;
+			$desc = $data->description;
 
+		}
 		if ($debug) {
 			error_log('[SMS] REQUEST: ' .  print_r($data, true));
 		}
-		route_and_send_sms($data->from, $data->to, $data->text);
+
+		/**
+		 * Bandwidth uses HTTP Callbacks webhooks to send events to any publicly addressable url, as defined in 
+		 * your messaging application. All Message callbacks are sent as a list/array [ {message metadata} ] 
+		 * to the webhook url in the application. You MUST Reply with a HTTP 2xx status code for every 
+		 * callback/delivery receipt. Bandwidth will retry every callback over the next 24 hours until a HTTP 2xx 
+		 * code is received for the callback. After 24 hours, Bandwidth will no longer try to send the callback.
+		 * Bandwidth's Messaging platform has a 10 second timeout for callbacks. This means your server must 
+		 * respond to the callback request within 10 seconds, otherwise the platform will try again at a later time.
+		 * 
+		 * https://dev.bandwidth.com/docs/messaging/webhooks#message-failed/
+		 */
+
+		switch ($msg_type) {
+			case 'message-delivered': {
+				route_and_send_sms($from, $to, $text);			
+				return http_response_code(200);
+				break;
+			}
+			case 'message-failed': {
+				$text .= $data->description . '\n' . $text;
+				route_and_send_sms($from, $to, $text);
+				return http_response_code(200);
+				break;
+			}
+			default:
+				route_and_send_sms($from, $to, $text);			
+				return http_response_code(200);
+				break;
+		}
 	} else {
 		error_log('[SMS] REQUEST: No SMS Data Received');
 		die("no");
