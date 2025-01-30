ac5eef633b5e0d028399bbdbd5b32a6ffee608f3
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-01-30 10:52:41 -0700

    Mobile Twinning - Fix errors and add card and counter divs (#189)
    * Fix errors and add card and counter divs
    
    * Update mobile_twinning_edit.php

--- mobile-twinning/mobile_twinning.php
+++ mobile-twinning/mobile_twinning.php
@@ -26,12 +26,13 @@
 */
 
 //includes
-	include "root.php";
-	require_once "resources/require.php";
+	require_once dirname(__DIR__, 2) . "/resources/require.php";
+	require_once "resources/pdo.php";
 	require_once "resources/check_auth.php";
 	require_once "resources/paging.php";
+	require_once "resources/header.php";
 
-//check permissions	
+//check permissions
 	if (permission_exists('mobile_twinning_view')) {
 		//access granted
 	}
@@ -43,7 +44,7 @@
 //add multi-lingual support
 	$language = new text;
 	$text = $language->get();
-	
+
 //get the https values and set as variables
 	$order_by = check_str($_GET["order_by"]);
 	$order = check_str($_GET["order"]);
@@ -89,7 +90,7 @@
 	$sql .= "FROM  v_extensions AS e \n ";
 	$sql .= "LEFT OUTER JOIN v_mobile_twinnings AS m ON m.extension_uuid = e.extension_uuid ";
 	$sql .= "where e.domain_uuid = '$domain_uuid' ";
-	$sql .= $sql_mod; //add search mod from above	
+	$sql .= $sql_mod; //add search mod from above
 	$sql .= "and e.enabled = 'true' ";
 	if (strlen($order_by)> 0) {
 		$sql .= "order by $order_by $order ";
@@ -110,35 +111,27 @@
 	$row_style["1"] = "row_style1";
 
 //begin the content
-	require_once "resources/header.php";
-	echo "<table width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n";
-	echo "  <tr>\n";
-	echo "	<td align='left' width='100%'>\n";
-	echo "		<b>".$text['header-mobile_twinning']." (".$numeric_extensions.")</b><br>\n";
-	echo "	</td>\n";
-	echo "		<td align='right' width='100%' style='vertical-align: top;'>";
+	echo "<div class='action_bar' id='action_bar'>\n";
+	echo "	<div class='heading'><b>".$text['header-mobile_twinning']."</b><div class='count'>".number_format($numeric_extensions)."</div></div>\n";
+	echo "	<div class='actions'>\n";
 	if ((if_group("admin") || if_group("superadmin"))) {
-		echo "		<form method='get' action=''>\n";
-		echo "			<td style='vertical-align: top; text-align: right; white-space: nowrap;'>\n";
-		echo "				<input type='text' class='txt' style='width: 150px' name='search' id='search' value='".$search."'>";
-		echo "				<input type='submit' class='btn' name='submit' value='".$text['button-search']."'>";
+		echo "	<form method='get' action=''>\n";
+		echo "		<input type='text' class='txt' style='width: 150px' name='search' id='search' value='".$search."'>";
+		echo "		<input type='submit' class='btn' name='submit' value='".$text['button-search']."'>";
 		if ($paging_controls_mini != '') {
-			echo 			"<span style='margin-left: 15px;'>".$paging_controls_mini."</span>\n";
+			echo 	"<span style='margin-left: 15px;'>".$paging_controls_mini."</span>\n";
 		}
-		echo "			</td>\n";
-		echo "			</td>\n";
-		echo "		</form>\n";	
+		echo "	</form>\n";
 	}
-	echo "  </tr>\n";
 
-	echo "	<tr>\n";
-	echo "		<td colspan='2'>\n";
-	echo "			".$text['description-mobile_twinning']."\n";
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "</table>\n";
-	echo "<br>";
-	
+	echo "	</div>\n";
+	echo "	<div style='clear: both;'></div>\n";
+	echo "</div>\n";
+
+	echo $text['description-mobile_twinning']."\n";
+	echo "<br /><br />";
+
+	echo "<div class='card'>\n";
 	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 	echo "<tr>\n";
 	echo th_order_by('extension', $text['table-extension'], $order_by,$order);
@@ -155,7 +148,7 @@
 			echo "	<td valign='top' class='row_stylebg' width='40%'>".$row['description']."&nbsp;</td>\n";
 			echo "	<td class='list_control_icons'>";
 			echo "     <a href='mobile_twinning_edit.php?id=".$row['mobile_twinning_uuid']."&extid=".$row['extension_uuid']."'>$v_link_label_edit</a>";
-			echo "  </td>\n";			
+			echo "  </td>\n";
 			echo "</tr>\n";
 			if ($c==0) { $c=1; } else { $c=0; }
 		} //end foreach
@@ -163,12 +156,13 @@
 	} //end if results
 
 	echo "</table>";
+	echo "</div>\n";
 	if (strlen($paging_controls) > 0) {
 		echo "<br />";
 		echo $paging_controls."\n";
 	}
 	echo "<br><br>";
-	
+
 //show the footer
 	require_once "resources/footer.php";
 ?>
